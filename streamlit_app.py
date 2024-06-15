6d2de2488fb5e02257e9c466f793e0079033f1d7
Khaled L <khaled.laywad@gmail.com>
2024-06-15 15:33:07 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,13 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import pickle as pkl
 
 st.write("Hellow worlds")
-monthlyIncome = st.number_input ("Monthly Income ($)",0,500000,1000,1)
-maritalStatus = st.selectbox("Marital Status",("Single","Married"))
-environmentSatisfaction = st.selectbox("Environment Satisfaction",("Very Satisfied","Satisfied","Disatisfied","Very Disatisfied"))
-yearsWithCurrentManager = st.number_input ("No. Years With Current Manager",0,20,1,1)
-st.write('test')
+with st.sidebar:
+    monthlyIncome = st.number_input ("Monthly Income ($)",0,500000,1000,1)
+    maritalStatus = st.selectbox("Marital Status",("Single","Married"))
+    environmentSatisfaction = st.selectbox("Environment Satisfaction",("Very Satisfied","Satisfied","Disatisfied","Very Disatisfied"))
+    yearsWithCurrentManager = st.number_input ("No. Years With Current Manager",0,20,1,1)
+    Age = st.number_input ("Age",18,60,20,1)
+    jobSatisfaction = st.selectbox("Job Satisfaction",("Very Satisfied","Satisfied","Disatisfied","Very Disatisfied"))
