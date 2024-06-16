2343dfa98cf7d69cab3efb22f87ede6d7cc0aa1c
Khaled L <khaled.laywad@gmail.com>
2024-06-17 00:18:33 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,6 +72,7 @@ if submit_button:
        'YearsInCurrentRole' : yearsInCurrentRole,
          'YearsSinceLastPromotion' : yearsSinceLastPromotion,
        'YearsWithCurrManager' : yearsWithCurrentManager}
+    st.write(data)
     df = pd.DataFrame(data)
     model_rf = pkl.load(open("random-forest-attrition.pkl", "rb"))
     turnover_prediction = model_rf.predict_proba(df)
