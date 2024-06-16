0725f22f984f1362c694bc4720058a5a4576b4c0
Khaled L <khaled.laywad@gmail.com>
2024-06-17 01:33:17 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,13 @@ with st.form(key='my_form'):
             distanceFromeHome = st.slider("Distance From Home (KM)",1,200,2,1)
             totalWorkingYears = st.slider("Total Working Years",yearsAtCompnay,60,1,yearsAtCompnay)
             yearsInCurrentRole = st.slider("Years In Current Role",0,60,1,1)
-            workLifeBalance = st.slider("Work Life Balance",1,4,1,2)
-            jobInvolvement = st.slider("Job Involvement",1,4,1,2)
-            relationShipSatisfaction = st.slider("Relationship Satisfaction",1,4,1,2)
+            workLifeBalance = st.slider("Work Life Balance",1,4,2,1)
+            jobInvolvement = st.slider("Job Involvement",1,4,2,1)
+            relationShipSatisfaction = st.slider("Relationship Satisfaction",1,4,2,1)
             department =st.selectbox("Department",("Sales","HR","R&D"))
             jobLevel = st.slider("Job Level",1,5,1,2)
-            trainingTimesLastYear = st.slider("Training Times Last year",1,10,1,2)
-            percentSalaryHike = st.slider("Percentage Increase In Salary ",1,30,1,5)
+            trainingTimesLastYear = st.slider("Training Times Last year",1,10,2,1)
+            percentSalaryHike = st.slider("Percentage Increase In Salary ",1,30,5,1)
             numCompaniesWorked = st.slider("Number of Compnaies Worked",1,10,1,1)
             education = st.slider("Education",1,5,1,3)
             yearsSinceLastPromotion = st.slider("Years Since Last Promotion",1,yearsAtCompnay,1,1)
@@ -34,7 +34,7 @@ with st.form(key='my_form'):
             jobRole = st.selectbox("Job Role",("HC REP","HR","LAB TECHNICIAN","MANAGER","MANAGING DIRECTOR","REASEARCH DIRECTOR","RESEARCH SCIENTIST","SALES EXECUTIEVE","SALES REPRESENTATIVE"))
             gender = st.selectbox("Gender",("Male","Female"))
             overTime =st.selectbox("OverTime",("Yes","No"))
-            performanceRating = st.slider("Performance Rating",1,5,1,3)
+            performanceRating = st.slider("Performance Rating",1,5,3,1)
 if submit_button:
     data = {'Age' : Age,
             'BusinessTravel_Travel_Frequently' : businessTravel == "Travel Frequently", 
