8332f4dff292398499c0bdfe3bcd293f7def49ea
Khaled L <khaled.laywad@gmail.com>
2024-06-17 01:26:05 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,6 +77,6 @@ if submit_button:
     model_rf = pd.read_pickle("random-forest-attrition.pkl")
     turnover_prediction = model_rf.predict_proba(df)
     st.write(turnover_prediction)
-    st.write(turnover_prediction[0][1])
+    st.write('There is a', turnover_prediction[0][1] * 100 ,'% chance of the employee leaving')
   
 
