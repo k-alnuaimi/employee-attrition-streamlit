c89fbfdc23158b43d0ecbfb4dcb80974e6540366
Khaled L <khaled.laywad@gmail.com>
2024-06-17 21:53:54 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,41 @@
 import pandas as pd
 import streamlit as st
-import pickle as pkl
 
+@st.cache_resource
+def init_model():
+    return pd.read_pickle("random-forest-attrition.pkl")
+model_rf = init_model()
 
-
+monthlyIncome = st.number_input ("Monthly Income ($)",0,500000,1000,1)
+maritalStatus = st.selectbox("Marital Status",("Single","Married","Divorced"))
+satisfactionOptions = ["Very Disatisfied","Disatisfied","Satisfied","Very Satisfied"]
+environmentSatisfaction = st.selectbox("Environment Satisfaction",satisfactionOptions)
+yearsAtCompnay = st.number_input ("No. Years At Company",0,50,5,1)
+yearsWithCurrentManager = st.number_input ("No. Years With Current Manager",0,yearsAtCompnay,2,1)
+Age = st.number_input ("Age",18,60,20,1)
+jobSatisfaction = st.selectbox("Job Satisfaction",satisfactionOptions)
 with st.form(key='my_form'):
     with st.sidebar:
-        monthlyIncome = st.number_input ("Monthly Income ($)",0,500000,1000,1)
-        maritalStatus = st.selectbox("Marital Status",("Single","Married","Divorced"))
-        satisfactionOptions = ["Very Disatisfied","Disatisfied","Satisfied","Very Satisfied"]
-        environmentSatisfaction = st.selectbox("Environment Satisfaction",satisfactionOptions)
-        yearsAtCompnay = st.number_input ("No. Years At Company",0,50,5,1)
-        yearsWithCurrentManager = st.number_input ("No. Years With Current Manager",0,yearsAtCompnay,2,1)
-        Age = st.number_input ("Age",18,60,20,1)
-        jobSatisfaction = st.selectbox("Job Satisfaction",satisfactionOptions)
         submit_button = st.form_submit_button("Submit")
-        with st.expander("Additional Fields"):
-            #don't forget to convert to miles ( multiply by 0.6 )
-            distanceFromeHome = st.slider("Distance From Home (KM)",1,200,2,1)
-            totalWorkingYears = st.slider("Total Working Years",yearsAtCompnay,60,1,yearsAtCompnay)
-            yearsInCurrentRole = st.slider("Years In Current Role",0,60,1,1)
-            workLifeBalance = st.slider("Work Life Balance",1,4,2,1)
-            jobInvolvement = st.slider("Job Involvement",1,4,2,1)
-            relationShipSatisfaction = st.slider("Relationship Satisfaction",1,4,2,1)
-            department =st.selectbox("Department",("Sales","HR","R&D"))
-            jobLevel = st.slider("Job Level",1,5,1,2)
-            trainingTimesLastYear = st.slider("Training Times Last year",1,10,2,1)
-            percentSalaryHike = st.slider("Percentage Increase In Salary ",1,30,5,1)
-            numCompaniesWorked = st.slider("Number of Compnaies Worked",1,10,1,1)
-            education = st.slider("Education",1,5,1,3)
-            yearsSinceLastPromotion = st.slider("Years Since Last Promotion",1,yearsAtCompnay,1,1)
-            businessTravel = st.selectbox("Business Travel",("No Travel","Travel Frequently","Travel Rarely"))
-            jobRole = st.selectbox("Job Role",("HC REP","HR","LAB TECHNICIAN","MANAGER","MANAGING DIRECTOR","REASEARCH DIRECTOR","RESEARCH SCIENTIST","SALES EXECUTIEVE","SALES REPRESENTATIVE"))
-            gender = st.selectbox("Gender",("Male","Female"))
-            overTime =st.selectbox("OverTime",("Yes","No"))
-            performanceRating = st.slider("Performance Rating",1,5,3,1)
+        #don't forget to convert to miles ( multiply by 0.6 )
+        distanceFromeHome = st.slider("Distance From Home (KM)",1,200,2,1)
+        totalWorkingYears = st.slider("Total Working Years",yearsAtCompnay,60,1,yearsAtCompnay)
+        yearsInCurrentRole = st.slider("Years In Current Role",0,60,1,1)
+        workLifeBalance = st.slider("Work Life Balance",1,4,2,1)
+        jobInvolvement = st.slider("Job Involvement",1,4,2,1)
+        relationShipSatisfaction = st.slider("Relationship Satisfaction",1,4,2,1)
+        department =st.selectbox("Department",("Sales","HR","R&D"))
+        jobLevel = st.slider("Job Level",1,5,1,2)
+        trainingTimesLastYear = st.slider("Training Times Last year",1,10,2,1)
+        percentSalaryHike = st.slider("Percentage Increase In Salary ",1,30,5,1)
+        numCompaniesWorked = st.slider("Number of Compnaies Worked",1,10,1,1)
+        education = st.slider("Education",1,5,1,3)
+        yearsSinceLastPromotion = st.slider("Years Since Last Promotion",1,yearsAtCompnay,1,1)
+        businessTravel = st.selectbox("Business Travel",("No Travel","Travel Frequently","Travel Rarely"))
+        jobRole = st.selectbox("Job Role",("HC REP","HR","LAB TECHNICIAN","MANAGER","MANAGING DIRECTOR","REASEARCH DIRECTOR","RESEARCH SCIENTIST","SALES EXECUTIEVE","SALES REPRESENTATIVE"))
+        gender = st.selectbox("Gender",("Male","Female"))
+        overTime =st.selectbox("OverTime",("Yes","No"))
+        performanceRating = st.slider("Performance Rating",1,5,3,1)
 if submit_button:
     data = {'Age' : Age,
             'BusinessTravel_Travel_Frequently' : businessTravel == "Travel Frequently", 
@@ -72,9 +73,7 @@ if submit_button:
          'YearsSinceLastPromotion' : yearsSinceLastPromotion,
        'YearsWithCurrManager' : yearsWithCurrentManager}
     df = pd.DataFrame(data,index=[0])
-    st.write(len(df.columns.tolist()))
     df.replace({False: 0, True: 1}, inplace=True)
-    model_rf = pd.read_pickle("random-forest-attrition.pkl")
     turnover_prediction = model_rf.predict_proba(df)
     st.write(turnover_prediction)
     st.write('There is a', turnover_prediction[0][1] * 100 ,'% chance of the employee leaving')
