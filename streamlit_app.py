cbb11555af1606763b1843856fdfe971b5127153
Khaled L <khaled.laywad@gmail.com>
2024-06-21 10:52:57 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,39 +53,39 @@ showrate = False
     
 
 col1, col2, col3 = st.columns(3)
-with st.form(key='my_form'):
-    with col1:
-        monthlyIncome = st.slider("Monthly Income ($)",0,100000,6500,500)
-        maritalStatus = st.selectbox("Marital Status",("Single","Married","Divorced"))
-        satisfactionOptions = ["Very Disatisfied","Disatisfied","Satisfied","Very Satisfied"]
-    with col2:
-        environmentSatisfaction = st.selectbox("Environment Satisfaction",satisfactionOptions,2)
-        yearsAtCompnay = st.number_input ("No. Years At Company",0,50,5,1)
-        yearsWithCurrentManager = st.number_input ("No. Years With Current Manager",0,50,3,1)
-    with col3:
-        Age = st.number_input ("Age",18,60,35,1)
-        jobSatisfaction = st.selectbox("Job Satisfaction",satisfactionOptions,1)
-        distanceFromeHome = st.slider("Distance From Home (KM)",1,200,4,1)
-    with st.sidebar:
-        
-        #don't forget to convert to miles ( multiply by 0.6 )
-        totalWorkingYears = st.slider("Total Working Years",0,60,6,1)
-        yearsInCurrentRole = st.slider("Years In Current Role",0,60,3,1)
-        workLifeBalance = st.slider("Work Life Balance",1,4,1,1)
-        jobInvolvement = st.slider("Job Involvement",1,4,2,1)
-        relationShipSatisfaction = st.slider("Relationship Satisfaction",1,4,2,1)
-        department =st.selectbox("Department",("Sales","HR","R&D"))
-        jobLevel = st.slider("Job Level",1,5,2,1)
-        trainingTimesLastYear = st.slider("Training Times Last year",1,10,2,1)
-        percentSalaryHike = st.slider("Percentage Increase In Salary ",1,30,5,1)
-        numCompaniesWorked = st.slider("Number of Compnaies Worked",1,10,2,1)
-        education = st.slider("Education",1,5,3,1)
-        yearsSinceLastPromotion = st.slider("Years Since Last Promotion",1,30,2,1)
-        businessTravel = st.selectbox("Business Travel",("No Travel","Travel Frequently","Travel Rarely"))
-        jobRole = st.selectbox("Job Role",("HC REP","HR","LAB TECHNICIAN","MANAGER","MANAGING DIRECTOR","REASEARCH DIRECTOR","RESEARCH SCIENTIST","SALES EXECUTIEVE","SALES REPRESENTATIVE"))
-        gender = st.selectbox("Gender",("Male","Female"))
-        overTime =st.selectbox("OverTime",("Yes","No"))
-        performanceRating = st.slider("Performance Rating",1,5,2,1)
+#with st.form(key='my_form'):
+with col1:
+    monthlyIncome = st.slider("Monthly Income ($)",0,100000,6500,500)
+    maritalStatus = st.selectbox("Marital Status",("Single","Married","Divorced"))
+    satisfactionOptions = ["Very Disatisfied","Disatisfied","Satisfied","Very Satisfied"]
+with col2:
+    environmentSatisfaction = st.selectbox("Environment Satisfaction",satisfactionOptions,2)
+    yearsAtCompnay = st.number_input ("No. Years At Company",0,50,5,1)
+    yearsWithCurrentManager = st.number_input ("No. Years With Current Manager",0,50,3,1)
+with col3:
+    Age = st.number_input ("Age",18,60,35,1)
+    jobSatisfaction = st.selectbox("Job Satisfaction",satisfactionOptions,1)
+    distanceFromeHome = st.slider("Distance From Home (KM)",1,200,4,1)
+with st.sidebar:
+    
+    #don't forget to convert to miles ( multiply by 0.6 )
+    totalWorkingYears = st.slider("Total Working Years",0,60,6,1)
+    yearsInCurrentRole = st.slider("Years In Current Role",0,60,3,1)
+    workLifeBalance = st.slider("Work Life Balance",1,4,1,1)
+    jobInvolvement = st.slider("Job Involvement",1,4,2,1)
+    relationShipSatisfaction = st.slider("Relationship Satisfaction",1,4,2,1)
+    department =st.selectbox("Department",("Sales","HR","R&D"))
+    jobLevel = st.slider("Job Level",1,5,2,1)
+    trainingTimesLastYear = st.slider("Training Times Last year",1,10,2,1)
+    percentSalaryHike = st.slider("Percentage Increase In Salary ",1,30,5,1)
+    numCompaniesWorked = st.slider("Number of Compnaies Worked",1,10,2,1)
+    education = st.slider("Education",1,5,3,1)
+    yearsSinceLastPromotion = st.slider("Years Since Last Promotion",1,30,2,1)
+    businessTravel = st.selectbox("Business Travel",("No Travel","Travel Frequently","Travel Rarely"))
+    jobRole = st.selectbox("Job Role",("HC REP","HR","LAB TECHNICIAN","MANAGER","MANAGING DIRECTOR","REASEARCH DIRECTOR","RESEARCH SCIENTIST","SALES EXECUTIEVE","SALES REPRESENTATIVE"))
+    gender = st.selectbox("Gender",("Male","Female"))
+    overTime =st.selectbox("OverTime",("Yes","No"))
+    performanceRating = st.slider("Performance Rating",1,5,2,1)
 
    # if submit_button:
   #    showrate = True
