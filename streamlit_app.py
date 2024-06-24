dea1b1a8e7ba103f6fa3cb8a8bd55bb61b2ec8cc
Khaled L <khaled.laywad@gmail.com>
2024-06-24 08:52:47 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ def show_turnover_rate():
     else:
         text = 'Employee likely to leave'
         img = 'Sad Employee 1.jpeg'
-    col1, col2 = st.columns(3,[0.4,0.6])
+    col1, col2 = st.columns([0.4,0.6])
     with col1:
         st.write(text)
     with col2:
